21367126e86d79aacf2ee4b0306e473ea1602f68
Andreas Martin <andreas.martin@andreasmartin.ch>
2019-11-28 14:43:27 +0100

    Update

--- src/main/java/ai/convers/dialogflow/DialogflowActionApplication.java
+++ src/main/java/ai/convers/dialogflow/DialogflowActionApplication.java
@@ -7,7 +7,9 @@ package ai.convers.dialogflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class DialogflowActionApplication {
 
